10628a02c3bb37e7574c46463c7e1bb7e416b6f7
Prachi-6921 <prachi02rani@gmail.com>
2026-09-05 09:08:03 +0530

    [Prachi Rani]: added csv parser code

---
+++ src/main/java/string/assignment_problems/L3_ProductInventoryCSVParser.java
@@ -0,0 +1,36 @@
+package string.assignment_problems;
+
+import java.util.Scanner;
+
+public class L3_ProductInventoryCSVParser {
+
+    static void parseInventoryRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+
+        String product = fields[0];
+        String sku = fields[1];
+        String quantity = fields[2];
+
+        System.out.println("Product: " + product
+                + " | SKU: " + sku
+                + " | Qty: " + quantity);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter inventory record: ");
+        String csvLine = sc.nextLine();
+
+        parseInventoryRecord(csvLine);
+
+        sc.close();
+    }
+}
