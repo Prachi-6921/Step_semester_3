d969d3e50443de6b7cd6bf715775e5562ceaaf87
Prachi-6921 <prachi02rani@gmail.com>
2026-09-19 09:50:32 +0530

    [Prachi Rani] added student placement record mangement code

---
+++ src/main/java/classes_objects/class_problems/M1_StudentPlacementRecordManagement.java
@@ -0,0 +1,35 @@
+package classes_objects.class_problems;
+
+public class M1_StudentPlacementRecordManagement {
+
+    static class PlacementRecord {
+
+        String studentName;
+        String company;
+        double packageLpa;
+
+        PlacementRecord(String studentName, String company, double packageLpa) {
+            this.studentName = studentName;
+            this.company = company;
+            this.packageLpa = packageLpa;
+        }
+
+        void printRecord() {
+            System.out.println(studentName + " -> " + company
+                    + " @ " + packageLpa + " LPA");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PlacementRecord[] records = {
+                new PlacementRecord("Ravi", "TCS", 4.5),
+                new PlacementRecord("Anitha", "Zoho", 6.2),
+                new PlacementRecord("Karthik", "Infosys", 4.0)
+        };
+
+        for (int i = 0; i < records.length; i++) {
+            records[i].printRecord();
+        }
+    }
+}
